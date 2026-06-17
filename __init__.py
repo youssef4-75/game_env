8d5c98c5ab1064902fc795df54eca16481a1c42f
youssef4-75 <youssefhaddadi115@gmail.com>
2026-06-17 22:09:10 +0100

    added the init statement

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
+print("Made by a developer, for developers who don't want to write the same boilerplate for the 47th time.")
 
 from .game import *
 from .objects import *
 from .utils import *
 from .plugins import *
-
-print("hello there")
